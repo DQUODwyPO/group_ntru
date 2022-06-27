6750679f52e7d60d77d453f0deb607ebc69b4290
Dquodwypo <dquodwypo@yandex.ru>
2022-06-27 23:29:27 +0300

    Каркас для приложения.

---
+++ GNTRUSign.py
@@ -0,0 +1,18 @@
+
+
+class NTRUSign:
+
+    def __init__(self):
+        pass
+
+    def KeyGen(self):
+        pass
+
+    def Sign(self, message: bytes) -> bytes:
+        pass
+
+    def Vrfy(self, signature: bytes) -> bool:
+        pass
+
+    def Open(self):
+        pass
---
+++ main.py
@@ -0,0 +1,10 @@
+import GNTRUSign
+import tkinter
+
+
+def main():
+    pass
+
+
+if __name__ == "__main__":
+    main()
